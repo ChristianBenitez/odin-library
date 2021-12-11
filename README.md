f988a9a051ba8b82832c85bf98a0e17eb1385456
Christian Benitez <10christian@live.com>
2021-12-11 15:54:49 -0800

    fixed read error

---
+++ README.md
@@ -0,0 +1,8 @@
+Steps to complete project
+    1. Create a constructor Book
+    2. Create a Form in HTML to collect user input
+    3. Create a function to retrieve data from form
+    4. Use data to create a new object
+    5. Place new object into Array myLibrary
+    6. Display all objects in myLibrary
+    7. 
--- Script.js
+++ Script.js
@@ -1,6 +1,13 @@
- let myLibrary = [];
+let myLibrary = [
+	{
+		"title" : "Harry Potter",
+		"author" : "J.K. Rowling",
+		"pages" : 255,
+		"read" : "not read yet"
+	}
+];
 
- function Book (title, author, pages, read){
+var Book = function (title, author, pages, read){
 	this.title = title,
 	this.author = author,
 	this.pages = pages;
@@ -9,14 +16,20 @@
 	} else {
 		this.read = "not read yet"
 	};
-	console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
- }
+}
 
- function addBookToLibrary(){
-	var T = document.getElementById('title').value;
-	this.T =Book (document.getElementById('title').value,
-				  document.getElementById('author').value,
-				  document.getElementById('pages').value,
-				  document.getElementById('read').value);
-	myLibrary.push();
- }
+function addBookToLibrary(){
+	var newBook = new Book (document.getElementById('title').value,
+					document.getElementById('author').value,
+					document.getElementById('pages').value,
+					document.getElementById('read').checked);
+	console.log(newBook);
+	myLibrary.push(newBook);
+}
+
+var testBook = {
+	"title" : "Statistics for dummies",
+	"author" : "Arthur",
+	"pages" : 100,
+	"read" : "has not read",
+}
