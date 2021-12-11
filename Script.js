dc9cea817fe790cffa718484a116e70328e632fa
Christian Benitez <10christian@live.com>
2021-12-10 20:14:13 -0800

    HTML form manipulation complete

--- Script.js
+++ Script.js
@@ -3,17 +3,20 @@
  function Book (title, author, pages, read){
 	this.title = title,
 	this.author = author,
-	this.pages = pages,
-	this.read = read,
-	this.info() = function() {
-		return (`${this.title} by ${this.author}, ${this.pages}, ${this.read}`)
-	}
+	this.pages = pages;
+	if (read === true){
+		this.read = "already read"
+	} else {
+		this.read = "not read yet"
+	};
+	console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`)
  }
 
  function addBookToLibrary(){
 	var T = document.getElementById('title').value;
-	var A = document.getElementById('author').value;
-	var P = document.getElementById('pages').value;
-	
-
+	this.T =Book (document.getElementById('title').value,
+				  document.getElementById('author').value,
+				  document.getElementById('pages').value,
+				  document.getElementById('read').value);
+	myLibrary.push();
  }
